075a1d4e3f2415265347d5fd3c9b4bec83477966
Malhar <88683784+StoneTrooper66@users.noreply.github.com>
2021-08-14 13:50:35 +0530

    Update area of circle.py

--- area of circle.py
+++ area of circle.py
@@ -2,4 +2,12 @@ x=float(input("Input the radius of the circle: "))
 Area= 22/7*x*x
 print ("The area of the circle with radius "+ str(x) + " is " + str(Area))
 
+name = input("Input the Filename: ")
+ext = (".py")
+if ext in name:
+    print("The extension of the file is: 'python'")
+else:
+    print("Extension not identified!")
+
+
 
